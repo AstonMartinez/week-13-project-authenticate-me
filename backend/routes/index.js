d99510318d36141a344b38348d2e8a44d8ca3d87
Aston Martinez <aston.martinez1614@gmail.com>
2023-07-07 12:54:28 -0500

    finished phase 0

---
+++ backend/routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// require index.js from api router
+// const apiRouter = require('./api')
+// router.use('/api', apiRouter)
+
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-TOKEN': csrfToken
+    });
+});
+
+module.exports = router;
