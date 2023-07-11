c390db738018eca055d9101c5030e8d50b54e08c
Aston Martinez <aston.martinez1614@gmail.com>
2023-07-10 20:43:32 -0500

    added missed routes to the session router

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -53,4 +53,25 @@ router.post('/', validateLogin, async (req, res, next) => {
     });
 })
 
+// log out
+router.delete('/', (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+});
+
+// get session user
+router.get('/', (req, res) => {
+    const { user } = req;
+    if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+    }
+    return res.json({
+        user: safeUser
+        });
+    } else return res.json({ user: null });
+});
+
 module.exports = router;
