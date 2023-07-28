e024f93d20682c3f24c30844d508c17af528b8e0
Aston Martinez <aston.martinez1614@gmail.com>
2023-07-28 16:04:51 -0500

    fixed login component import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
-import LoginFormPage from './components/LoginFormPage'
+// import LoginFormPage from './components/LoginFormPage'
+import LoginFormPage from './components/LoginFormPage/index'
 import * as sessionActions from './store/session'
 
 function App() {
